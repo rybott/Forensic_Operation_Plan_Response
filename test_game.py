9b7b8b9905c409018f6b6673ae6b1426303a0439
Troop284 <rybott5000.5@gmail.com>
2023-09-14 16:08:45 -0400

    Game

--- test_game.py
+++ test_game.py
@@ -9,25 +9,24 @@ screen = pygame.display.set_mode((ScrWidth, ScrHeight))
 
 # Create a rectangle((Position, Position, Size, Size))
 player = pygame.Rect((300,250,50,50))
-
 run = True
-While run:
 
+while run == True:
   # Screen color 
   screen.fill((0,0,0))
 
   pygame.draw.rect(screen, (255, 0, 0), player)
 
-  key = pygame.key.get_presssed()
+  key = pygame.key.get_pressed()
   if key[pygame.K_a] == True:
     player.move_ip(-1,0)
     # If user hits a, then the player will move left
   elif key[pygame.K_d] == True:
     player.move_ip(1,0)
   elif key[pygame.K_s] == True:
-    player.move_ip(0,-1)
-  elif key[pygame.K_w] == True:
     player.move_ip(0,1)
+  elif key[pygame.K_w] == True:
+    player.move_ip(0,-1)
 
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
@@ -35,4 +34,4 @@ While run:
 
   pygame.display.update()
 
-pygame.quit()
+
